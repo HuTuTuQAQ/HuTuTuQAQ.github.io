b95bee441019029690b25cbc8f2be2d7679c3dd4
hututu <tutuhu903@gmail.com>
2024-07-13 23:46:10 +0800

    Site updated: 2024-07-13 23:46:06

--- js/runtime/runtime.js
+++ js/runtime/runtime.js
@@ -3,6 +3,8 @@ function createtime() {
   var grt = new Date("07/31/2023 00:00:00");
   now.setTime(now.getTime() + 250);
   var days = (now - grt) / 1e3 / 60 / 60 / 24,
+    t = Math.trunc(234e8 + (now - grt) / 1e3 * 17),
+    a = (t / 1496e5).toFixed(6),
     dnum = Math.floor(days),
     hours = (now - grt) / 1e3 / 60 / 60 - 24 * dnum,
     hnum = Math.floor(hours);
@@ -16,8 +18,8 @@ function createtime() {
   let currentTimeHtml = "";
   (currentTimeHtml =
     hnum < 18 && hnum >= 9
-      ? `<img class='boardsign' src='' title='距离月入25k也就还差一个大佬带我~'><span class='textTip'> <br> 本站居然运行了 ${dnum} 天</span><span id='runtime'> ${hnum} 小时 ${mnum} 分 ${snum} 秒 </span> <i class='fas fa-heartbeat' style='color:red'></i>`
-      : `<img class='boardsign' src='' title='下班了就该开开心心的玩耍，嘿嘿~'><span class='textTip'> <br> 本站居然运行了 ${dnum} 天</span><span id='runtime'> ${hnum} 小时 ${mnum} 分 ${snum} 秒 </span> <i class='fas fa-heartbeat' style='color:red'></i>`),
+      ? `<img class='boardsign' src='https://tutu-blog.obs.cn-southwest-2.myhuaweicloud.com/bolg/siticon/HuTuTu-%E7%BF%BB%E8%B7%9F%E6%96%97%E4%B8%AD-pink.svg' title='距离月入25k也就还差一个大佬带我~'><span class='textTip'> <br> 本站居然运行了 ${dnum} 天</span><span id='runtime'> ${hnum} 小时 ${mnum} 分 ${snum} 秒 </span> <i class='fas fa-heartbeat' style='color:red'></i> <br> 旅行者 1 号当前距离地球 ${t} 千米，约为 ${a} 个天文单位 🚀`
+      : `<img class='boardsign' src='https://tutu-blog.obs.cn-southwest-2.myhuaweicloud.com/bolg/siticon/HuTuTu-%E6%8C%81%E7%BB%AD%E5%8F%91%E5%91%86ing-pink.svg' title='下班了就该开开心心的玩耍，嘿嘿~'><span class='textTip'> <br> 本站居然运行了 ${dnum} 天</span><span id='runtime'> ${hnum} 小时 ${mnum} 分 ${snum} 秒 </span> <i class='fas fa-heartbeat' style='color:red'></i> <br> 旅行者 1 号当前距离地球 ${t} 千米，约为 ${a} 个天文单位 🚀`),
     document.getElementById("workboard") && (document.getElementById("workboard").innerHTML = currentTimeHtml);
 }
 setInterval(() => {
