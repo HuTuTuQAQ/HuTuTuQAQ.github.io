31118fad56172486bac44808cd69aad9f322c9a5
hututu <tutuhu903@gmail.com>
2024-10-01 03:31:20 +0800

    Site updated: 2024-10-01 03:31:11

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/7c2d.html","posts/63eb.html","posts/5942.html","posts/8b77.html","posts/762f.html"];function toRandomPost(){
+var posts=["posts/63eb.html","posts/7c2d.html","posts/5942.html","posts/f9c5.html","posts/8b77.html","posts/762f.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
