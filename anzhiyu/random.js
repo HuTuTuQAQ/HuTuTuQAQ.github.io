ababbb8482d0e1494ea3f550e82484bbf09a0e80
hututu <tutuhu903@gmail.com>
2024-12-22 02:00:28 +0800

    Site updated: 2024-12-22 02:00:19

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/63eb.html","posts/5942.html","posts/7c2d.html","posts/1a8.html","posts/278.html","posts/8b77.html","posts/ba7c.html","posts/f9c5.html","posts/762f.html"];function toRandomPost(){
+var posts=["posts/7c2d.html","posts/63eb.html","posts/5942.html","posts/1a8.html","posts/278.html","posts/8b77.html","posts/f9c5.html","posts/ba7c.html","posts/762f.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
