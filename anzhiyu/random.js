941c6f15bcd568ac39ad3945cfe2215313b2d458
hututu <tutuhu903@gmail.com>
2025-02-28 23:23:04 +0800

    Site updated: 2025-02-28 23:22:47

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/1a8.html","posts/b55e.html","posts/278.html","posts/8b77.html","posts/7c2d.html","posts/5942.html","posts/63eb.html","posts/ba7c.html","posts/f9c5.html","posts/762f.html"];function toRandomPost(){
+var posts=["posts/278.html","posts/1a8.html","posts/b55e.html","posts/8b77.html","posts/5942.html","posts/7c2d.html","posts/ba7c.html","posts/63eb.html","posts/f9c5.html","posts/762f.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
