fbd9b2fa331a60b9eeddbe6778277c5829f2b7e7
hututu <tutuhu903@gmail.com>
2025-03-01 00:22:18 +0800

    Site updated: 2025-03-01 00:22:03

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/1a8.html","posts/c50.html","posts/278.html","posts/b55e.html","posts/8b77.html","posts/5942.html","posts/63eb.html","posts/7c2d.html","posts/ba7c.html","posts/762f.html","posts/f9c5.html"];function toRandomPost(){
+var posts=["posts/1a8.html","posts/278.html","posts/8b77.html","posts/c50.html","posts/b55e.html","posts/5942.html","posts/63eb.html","posts/ba7c.html","posts/7c2d.html","posts/f9c5.html","posts/762f.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
