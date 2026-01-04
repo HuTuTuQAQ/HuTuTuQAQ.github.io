502c0922b02de17d9e7bfb3505bd1b5df7c42bd6
HuTuTuQAQ <tutuhu903@gmail.com>
2026-01-05 00:01:57 +0800

    Site updated: 2026-01-05 00:01:47

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/aba4.html","posts/1a8.html","posts/278.html","posts/8b77.html","posts/5942.html","posts/b55e.html","posts/63eb.html","posts/7c2d.html","posts/ba7c.html","posts/5f27.html","posts/f9c5.html","posts/762f.html"];function toRandomPost(){
+var posts=["posts/aba4.html","posts/1a8.html","posts/278.html","posts/8b77.html","posts/b55e.html","posts/5942.html","posts/63eb.html","posts/7c2d.html","posts/ba7c.html","posts/5f27.html","posts/f9c5.html","posts/762f.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
